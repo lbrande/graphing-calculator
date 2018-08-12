65d445a3ac9a3e158c9f943fab00a87b7d621690
lbrande <lovbra00@gmail.com>
2018-08-12 14:32:01 +0200

    Polar plots now work.

--- gui/src/main/java/se/lovebrandefelt/graphingcalculator/gui/GuiController.java
+++ gui/src/main/java/se/lovebrandefelt/graphingcalculator/gui/GuiController.java
@@ -4,13 +4,15 @@ import javafx.fxml.FXML;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Tooltip;
+import javafx.scene.layout.GridPane;
 import javafx.util.Duration;
 import se.lovebrandefelt.graphingcalculator.Function;
 
 public class GuiController {
   private static final String NUMERIC_VALUE_REGEX = "-?\\d+(?:\\.\\d+)?";
 
-  private static final String ILLEGAL_RANGE_ERROR_MESSAGE = "(%s:%s) is not a legal range";
+  private static final String NO_FUNCTION_TYPE_ERROR_MESSAGE = "No function type is selected.";
+  private static final String ILLEGAL_RANGE_ERROR_MESSAGE = "(%s:%s) is not a legal range.";
   private static final String NOT_NUMERIC_ERROR_MESSAGE = "\"%s\" is non-numeric.";
   private static final String NOT_POSITIVE_ERROR_MESSAGE = "\"%s\" is non-positive.";
 
@@ -18,58 +20,87 @@ public class GuiController {
   @FXML private TextField functionField;
   @FXML private RadioButton normalFunction;
   @FXML private RadioButton polarFunction;
+  @FXML private GridPane normalView;
   @FXML private TextField minX;
   @FXML private TextField maxX;
   @FXML private TextField minY;
   @FXML private TextField maxY;
   @FXML private TextField stepX;
+  @FXML private GridPane polarView;
   @FXML private TextField minT;
   @FXML private TextField maxT;
   @FXML private TextField stepT;
 
   @FXML
   private void initialize() {
-    updateView();
+    updateNormalView();
+    updatePolarView();
   }
 
   @FXML
   private void onFunctionChange() {
     unmarkFields(functionField);
 
-    if (functionField.getText().trim().isEmpty()) {
-      plotCanvas.removeFunction();
-    } else {
-      try {
-        if (normalFunction.isSelected()) {
-          plotCanvas.setFunction(new Function(functionField.getText(), 'x'), FunctionType.NORMAL);
-        } else if (polarFunction.isSelected()) {
-          plotCanvas.setFunction(new Function(functionField.getText(), 't'), FunctionType.POLAR);
-        }
-      } catch (IllegalArgumentException e) {
-        markField(functionField, e.getMessage());
-        plotCanvas.removeFunction();
+    try {
+      if (getFunctionType() == FunctionType.NORMAL) {
+        normalView.setDisable(false);
+        polarView.setDisable(true);
+      } else if (getFunctionType() == FunctionType.POLAR) {
+        normalView.setDisable(true);
+        polarView.setDisable(false);
       }
+      setFunction();
+    } catch (IllegalArgumentException e) {
+      markField(functionField, e.getMessage());
+      plotCanvas.removeFunction(getFunctionType());
     }
   }
 
   @FXML
   private void onViewChange() {
-    unmarkFields(minX, maxX, minY, maxY, stepX);
+    unmarkFields(minX, maxX, minY, maxY, stepX, minT, maxT, stepT);
 
     if (checkAndMarkRangeFields(minX, maxX)
         && checkAndMarkRangeFields(minY, maxY)
         && checkAndMarkPositiveField(stepX)) {
-      updateView();
+      updateNormalView();
+    }
+
+    if (checkAndMarkRangeFields(minT, maxT) && checkAndMarkPositiveField(stepT)) {
+      updatePolarView();
     }
   }
 
-  private void updateView() {
-    plotCanvas.setView(
+  private void setFunction() {
+    if (functionField.getText().trim().isEmpty()) {
+      plotCanvas.removeFunction(getFunctionType());
+    } else {
+      try {
+        plotCanvas.setFunction(
+            new Function(
+                functionField.getText(), getFunctionType() == FunctionType.NORMAL ? 'x' : 't'),
+            getFunctionType());
+      } catch (IllegalArgumentException e) {
+        markField(functionField, e.getMessage());
+        plotCanvas.removeFunction(getFunctionType());
+      }
+    }
+  }
+
+  private void updateNormalView() {
+    plotCanvas.setNormalView(
         Double.valueOf(minX.getText()),
         Double.valueOf(maxX.getText()),
         Double.valueOf(minY.getText()),
         Double.valueOf(maxY.getText()),
         Double.valueOf(stepX.getText()));
+  }
+
+  private void updatePolarView() {
+    plotCanvas.setPolarView(
+        Double.valueOf(minT.getText()),
+        Double.valueOf(maxT.getText()),
+        Double.valueOf(stepT.getText()));
   }
 
   private boolean checkAndMarkRangeFields(TextField min, TextField max) {
@@ -123,5 +154,14 @@ public class GuiController {
       field.getStyleClass().remove("error");
       field.setTooltip(null);
     }
+  }
+
+  private FunctionType getFunctionType() {
+    if (normalFunction.isSelected()) {
+      return FunctionType.NORMAL;
+    } else if (polarFunction.isSelected()) {
+      return FunctionType.POLAR;
+    }
+    throw new RuntimeException(NO_FUNCTION_TYPE_ERROR_MESSAGE);
   }
 }
--- gui/src/main/java/se/lovebrandefelt/graphingcalculator/gui/PlotCanvas.java
+++ gui/src/main/java/se/lovebrandefelt/graphingcalculator/gui/PlotCanvas.java
@@ -18,6 +18,9 @@ public class PlotCanvas extends Canvas {
   private double minY;
   private double maxY;
   private double stepX;
+  private double minT;
+  private double maxT;
+  private double stepT;
 
   void setFunction(Function function, FunctionType type) {
     this.function = function;
@@ -25,13 +28,13 @@ public class PlotCanvas extends Canvas {
     repaint();
   }
 
-  void removeFunction() {
+  void removeFunction(FunctionType newType) {
     function = null;
-    functionType = FunctionType.NORMAL;
+    functionType = newType;
     repaint();
   }
 
-  void setView(double minX, double maxX, double minY, double maxY, double stepX) {
+  void setNormalView(double minX, double maxX, double minY, double maxY, double stepX) {
     this.minX = minX;
     this.maxX = maxX;
     this.minY = minY;
@@ -40,18 +43,19 @@ public class PlotCanvas extends Canvas {
     repaint();
   }
 
+  void setPolarView(double minT, double maxT, double stepT) {
+    this.minT = minT;
+    this.maxT = maxT;
+    this.stepT = stepT;
+    repaint();
+  }
+
   private void repaint() {
     clear();
     if (functionType == FunctionType.NORMAL) {
-      paintNormalAxes();
-      if (function != null) {
-        paintNormalFunction();
-      }
+      paintNormalGraph();
     } else if (functionType == FunctionType.POLAR) {
-      paintPolarAxes();
-      if (function != null) {
-        paintPolarFunction();
-      }
+      paintPolarGraph();
     }
   }
 
@@ -59,10 +63,74 @@ public class PlotCanvas extends Canvas {
     getGraphicsContext2D().clearRect(0, 0, getWidth(), getHeight());
   }
 
-  private void paintNormalAxes() {
+  private void paintNormalGraph() {
+    paintAxes();
+    if (function != null) {
+      paintNormalFunction();
+    }
+  }
+
+  private void paintNormalFunction() {
+    getGraphicsContext2D().beginPath();
+    getGraphicsContext2D().moveTo(toScreenX(minX), toScreenY(function.evaluate(minX)));
+    for (var x = minX + stepX; x < maxX + stepX; x += stepX) {
+      getGraphicsContext2D().lineTo(toScreenX(x), toScreenY(function.evaluate(x)));
+    }
+    getGraphicsContext2D().setStroke(FUNCTION_COLOR);
+    getGraphicsContext2D().stroke();
+  }
+
+  private void paintPolarGraph() {
+    if (function != null) {
+      var xs = new double[(int) ((maxT - minT) / stepT + 1)];
+      var minX = Double.MAX_VALUE;
+      var maxX = Double.MIN_VALUE;
+      var ys = new double[(int) ((maxT - minT) / stepT + 1)];
+      var minY = Double.MAX_VALUE;
+      var maxY = Double.MIN_VALUE;
+      for (var i = 0; i <= (maxT - minT) / stepT; i++) {
+        var t = minT + i * stepT;
+        var r = function.evaluate(t);
+        xs[i] = r * Math.cos(t);
+        if (xs[i] < minX) {
+          minX = xs[i];
+        }
+        if (xs[i] > maxX) {
+          maxX = xs[i];
+        }
+        ys[i] = r * Math.sin(t);
+        if (ys[i] < minY) {
+          minY = ys[i];
+        }
+        if (ys[i] > maxY) {
+          maxY = ys[i];
+        }
+      }
+
+      paintAxes(minX, maxX, minY, maxY);
+      paintPolarFunction(xs, minX, maxX, ys, minY, maxY);
+    }
+  }
+
+  private void paintPolarFunction(
+      double[] xs, double minX, double maxX, double[] ys, double minY, double maxY) {
+    getGraphicsContext2D().beginPath();
+    getGraphicsContext2D().moveTo(toScreenX(xs[0], minX, maxX), toScreenY(ys[0], minY, maxY));
+    for (var i = 1; i < xs.length; i++) {
+      getGraphicsContext2D().lineTo(toScreenX(xs[i], minX, maxX), toScreenY(ys[i], minY, maxY));
+    }
+    getGraphicsContext2D().setStroke(FUNCTION_COLOR);
+    getGraphicsContext2D().stroke();
+  }
+
+  private void paintAxes() {
+    paintAxes(minX, maxX, minY, maxY);
+  }
+
+  private void paintAxes(double minX, double maxX, double minY, double maxY) {
     getGraphicsContext2D().setStroke(AXIS_COLOR);
     getGraphicsContext2D().setFill(AXIS_COLOR);
-    if (toScreenX(0) < 0 || toScreenX(0) > getWidth()) {
+    if (toScreenX(0, minX, maxX) < 0 || toScreenX(0, minX, maxX) > getWidth()) {
       getGraphicsContext2D().setTextAlign(TextAlignment.CENTER);
       getGraphicsContext2D().setTextBaseline(VPos.BOTTOM);
       getGraphicsContext2D().fillText(String.valueOf(minY), getWidth() / 2, getHeight());
@@ -70,11 +138,17 @@ public class PlotCanvas extends Canvas {
       getGraphicsContext2D().fillText(String.valueOf(maxY), getWidth() / 2, 0);
     } else {
       getGraphicsContext2D()
-          .strokeLine(toScreenX(0), toScreenY(minY), toScreenX(0), toScreenY(maxY));
+          .strokeLine(
+              toScreenX(0, minX, maxX),
+              toScreenY(minY, minY, maxY),
+              toScreenX(0, minX, maxX),
+              toScreenY(maxY, minY, maxY));
       getGraphicsContext2D()
           .fillPolygon(
               new double[] {
-                toScreenX(0) - AXIS_ARROW_SIZE / 2, toScreenX(0), toScreenX(0) + AXIS_ARROW_SIZE / 2
+                toScreenX(0, minX, maxX) - AXIS_ARROW_SIZE / 2,
+                toScreenX(0, minX, maxX),
+                toScreenX(0, minX, maxX) + AXIS_ARROW_SIZE / 2
               },
               new double[] {AXIS_ARROW_SIZE, 0, AXIS_ARROW_SIZE},
               3);
@@ -82,16 +156,17 @@ public class PlotCanvas extends Canvas {
       getGraphicsContext2D().setTextBaseline(VPos.BOTTOM);
       if (minY != 0) {
         getGraphicsContext2D()
-            .fillText(String.valueOf(minY), toScreenX(0) + AXIS_ARROW_SIZE / 2, getHeight());
+            .fillText(
+                String.valueOf(minY), toScreenX(0, minX, maxX) + AXIS_ARROW_SIZE / 2, getHeight());
       }
       getGraphicsContext2D().setTextBaseline(VPos.TOP);
       if (maxY != 0) {
         getGraphicsContext2D()
-            .fillText(String.valueOf(maxY), toScreenX(0) + AXIS_ARROW_SIZE / 2, 0);
+            .fillText(String.valueOf(maxY), toScreenX(0, minX, maxX) + AXIS_ARROW_SIZE / 2, 0);
       }
     }
 
-    if (toScreenY(0) < 0 || toScreenY(0) > getHeight()) {
+    if (toScreenY(0, minY, maxY) < 0 || toScreenY(0, minY, maxY) > getHeight()) {
       getGraphicsContext2D().setTextBaseline(VPos.CENTER);
       getGraphicsContext2D().setTextAlign(TextAlignment.LEFT);
       getGraphicsContext2D().fillText(String.valueOf(minX), 0, getHeight() / 2);
@@ -99,47 +174,48 @@ public class PlotCanvas extends Canvas {
       getGraphicsContext2D().fillText(String.valueOf(maxX), getWidth(), getHeight() / 2);
     } else {
       getGraphicsContext2D()
-          .strokeLine(toScreenX(minX), toScreenY(0), toScreenX(maxX), toScreenY(0));
+          .strokeLine(
+              toScreenX(minX, minX, maxX),
+              toScreenY(0, minY, maxY),
+              toScreenX(maxX, minX, maxX),
+              toScreenY(0, minY, maxY));
       getGraphicsContext2D()
           .fillPolygon(
               new double[] {getWidth() - AXIS_ARROW_SIZE, getWidth(), getWidth() - AXIS_ARROW_SIZE},
               new double[] {
-                toScreenY(0) - AXIS_ARROW_SIZE / 2, toScreenY(0), toScreenY(0) + AXIS_ARROW_SIZE / 2
+                toScreenY(0, minY, maxY) - AXIS_ARROW_SIZE / 2,
+                toScreenY(0, minY, maxY),
+                toScreenY(0, minY, maxY) + AXIS_ARROW_SIZE / 2
               },
               3);
       getGraphicsContext2D().setTextBaseline(VPos.BOTTOM);
       getGraphicsContext2D().setTextAlign(TextAlignment.LEFT);
       if (minX != 0) {
         getGraphicsContext2D()
-            .fillText(String.valueOf(minX), 0, toScreenY(0) - AXIS_ARROW_SIZE / 2);
+            .fillText(String.valueOf(minX), 0, toScreenY(0, minY, maxY) - AXIS_ARROW_SIZE / 2);
       }
       getGraphicsContext2D().setTextAlign(TextAlignment.RIGHT);
       if (maxX != 0) {
         getGraphicsContext2D()
-            .fillText(String.valueOf(maxX), getWidth(), toScreenY(0) - AXIS_ARROW_SIZE / 2);
+            .fillText(
+                String.valueOf(maxX), getWidth(), toScreenY(0, minY, maxY) - AXIS_ARROW_SIZE / 2);
       }
     }
   }
 
-  private void paintNormalFunction() {
-    getGraphicsContext2D().beginPath();
-    getGraphicsContext2D().moveTo(toScreenX(minX), toScreenY(function.evaluate(minX)));
-    for (var x = minX + stepX; x <= maxX + stepX; x += stepX) {
-      getGraphicsContext2D().lineTo(toScreenX(x), toScreenY(function.evaluate(x)));
-    }
-    getGraphicsContext2D().setStroke(FUNCTION_COLOR);
-    getGraphicsContext2D().stroke();
+  private double toScreenX(double x) {
+    return toScreenX(x, minX, maxX);
   }
 
-  private void paintPolarFunction() {}
-
-  private void paintPolarAxes() {}
-
-  private double toScreenX(double x) {
+  private double toScreenX(double x, double minX, double maxX) {
     return AXIS_ARROW_SIZE / 2 + (x - minX) / (maxX - minX) * (getWidth() - AXIS_ARROW_SIZE);
   }
 
   private double toScreenY(double y) {
+    return toScreenY(y, minY, maxY);
+  }
+
+  private double toScreenY(double y, double minY, double maxY) {
     return AXIS_ARROW_SIZE / 2
         + (getHeight() - AXIS_ARROW_SIZE)
         - (y - minY) / (maxY - minY) * (getHeight() - AXIS_ARROW_SIZE);
