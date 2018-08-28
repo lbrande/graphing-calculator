6f811044ece79a74be6b39a85f0f0ab43e08dcd9
lbrande <lovbra00@gmail.com>
2018-08-28 20:52:14 +0200

    Fixed names of exported modules.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,7 @@
 module se.lovebrandefelt.graphingcalculator {
+  requires org.junit.jupiter.api;
+  requires org.junit.jupiter.engine;
+
   exports se.lovebrandefelt.graphingcalculator;
   exports se.lovebrandefelt.graphingcalculator.token;
 }
