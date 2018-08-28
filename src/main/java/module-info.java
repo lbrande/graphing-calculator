fe96a149570263fa29c293aacd5f4282e52f0026
lbrande <lovbra00@gmail.com>
2018-08-28 21:30:29 +0200

    Fixed junit.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,4 @@
 module se.lovebrandefelt.graphingcalculator {
-  requires org.junit.jupiter.api;
-  requires org.junit.jupiter.engine;
-
   exports se.lovebrandefelt.graphingcalculator;
   exports se.lovebrandefelt.graphingcalculator.token;
 }
